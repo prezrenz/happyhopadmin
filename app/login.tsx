bde8ca831be5515da36064c88001547a36c79302
GahThong <anjopenaranda123@gmail.com>
2026-09-23 08:12:31 +0800

    hi

--- app/login.tsx
+++ app/login.tsx
@@ -1,18 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import { auth } from "../firebase.config";
-import { browserCookiePersistence, signInWithEmailAndPassword, getAuth } from "firebase/auth";
+import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
 
 export default function login() {
     const [email, setEmail] = useState("");
     const [pass, setPass] = useState("");
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
-    getAuth().onAuthStateChanged((user) => {
-        if (user) {
-            navigate("/admin");
-        }
-    })
+    useEffect(() => {
+        const unsubscribe = getAuth().onAuthStateChanged((user) => {
+            if (user) {
+                navigate("/admin");
+            }
+        });
+        return unsubscribe;
+    }, [navigate]);
 
     const handleLogin = async (e: any) => {
         e.preventDefault();
@@ -33,7 +36,7 @@ export default function login() {
     return (
         <div className="bg-orange-50 flex-col w-full text-center justify-center content-center items-center place-items-center">
             <div className="bg-orange-100 border p-4 rounded-2xl place-items-center">
-                <img className="object-center w-48 h-auto" src="/logo.png" />
+                <img className="object-center w-48 h-auto" src="app/assets/logo.png" />
                 <h1 className="font-bold text-2xl">Login</h1>
                 <h2 className="font-bold">Admin Email</h2>
                 <input className="bg-orange-200 border m-2 p-2 rounded-2xl" onChange={(e) => setEmail(e.target.value)} />
