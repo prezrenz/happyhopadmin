3554523ba6b3b5806f264ae1af86f496d3c8acc5
prezrenz <73829697+prezrenz@users.noreply.github.com>
2026-09-23 08:53:40 +0800

    Reapply "implement user verification types"
    This reverts commit 335ff1a55e277e6402dc53a3a738dddeaca055d4.

--- app/routes/admin/users.tsx
+++ app/routes/admin/users.tsx
@@ -50,8 +50,17 @@ export default function Reports({ loaderData }: Route.ComponentProps) {
         closeModal();
     }
 
-    const removeVerificationStatus = (id: string) => {
-        unverifyUserById(id);
+    const removeVerificationStatus = (id: string, type: string) => {
+        switch(type) {
+            case "verified":
+            case "verifiedVet":
+                unverifyUserById(id);
+                unverifyVetUserById(id);
+                break;
+            case "verifiedFeedSupplier":
+                unverifySupplierUserById(id);
+                break;
+        }
         closeUserModal();
     }
 
@@ -128,7 +137,7 @@ export default function Reports({ loaderData }: Route.ComponentProps) {
                                         <td>{user?.email}</td>
                                         <td>{user?.firstName + " " + user?.lastName}</td>
                                         <td>{user.username}</td>
-                                        <td>{user?.verified ? "✓" : "x"}</td>
+                                        <td>{user?.verified || user?.verifiedVet || user?.verifiedFeedSupplier ? "✓" : "x"}</td>
                                         <td><button onClick={() => openUserModal(user?.id)}>View Details</button></td>
                                     </tr>
                                 )
@@ -205,6 +214,10 @@ export default function Reports({ loaderData }: Route.ComponentProps) {
                             <b>Email: </b>
                             {currentUser?.email}
                         </div>
+                        <div className="flex flex-row">
+                            <b>Verifications: </b>
+                            {getVerifications(currentUser?.id)}
+                        </div>
                     </div>
                     {
                         (currentUser?.imageUrl) &&
@@ -229,9 +242,8 @@ export default function Reports({ loaderData }: Route.ComponentProps) {
                                 {currentPins.map((pin: any) => {
                                     return (
                                         <tr key={pin?.id}>
-                                            <td>{pin?.submittedAt?.toDate()?.toString()}</td>
                                             <td>{pin?.clinicName}</td>
-                                            <td>{pin?.type}</td>
+                                            <td>{(pin?.type === "vet") ? "Veterinary Clinic" : "Feed Supplier"}</td>
                                             <td>{pin?.latitude}</td>
                                             <td>{pin?.longitude}</td>
                                             <td><button onClick={() => deleteMapPin(pin?.id)}>Delete</button></td>
@@ -244,8 +256,12 @@ export default function Reports({ loaderData }: Route.ComponentProps) {
                 <div className="flex flex-row">
                     <button onClick={closeUserModal}>Close</button>
                     {
-                        (currentUser?.verified) &&
-                        <button onClick={() => removeVerificationStatus(currentUser?.id)}>Remove Verification</button>
+                        (currentUser?.verified || currentUser?.verifiedVet) &&
+                        <button onClick={() => removeVerificationStatus(currentUser?.id, "verified")}>Remove Veterinarian Verification</button>
+                    }
+                    {
+                        (currentUser?.verifiedFeedSupplier) &&
+                        <button onClick={() => removeVerificationStatus(currentUser?.id, "verifiedFeedSupplier")}>Remove Feed Supplier Verification</button>
                     }
                 </div>
             </WideModal>
